4d17bba48b68fe763ced7056b89d1797ab3278d9
Andrzej Gębuś <andrzejgebuss@gmail.com>
2021-03-14 09:59:04 +0100

    fix assets path

--- src/app/pipes/drink-img.pipe.ts
+++ src/app/pipes/drink-img.pipe.ts
@@ -7,17 +7,17 @@ import { drink } from '../dataModel/drink';
 export class DrinkImgPipe implements PipeTransform {
 
   imgMap: Map<string, string> = new Map()
-      .set("Ordinary Drink", "/assets/cocktail.svg")
-      .set("Cocktail", "/assets/cocktail-logo.svg")
-      .set("Milk / Float / Shake", "/assets/milkshake.svg")
-      .set("Other/Unknown", "/assets/cocktail-logo.svg")
-      .set("Cocoa", "/assets/cocoa.svg")
-      .set("Shot", "/assets/shot.svg")
-      .set("Coffee / Tea", "/assets/tea.svg")
-      .set("Homemade Liqueur", "/assets/cocktail.svg")
-      .set("Punch / Party Drink", "/assets/cocktail-straw.svg")
-      .set("Beer", "/assets/beer.svg")
-      .set("Soft Drink / Soda", "/assets/soft-drin.svg");
+      .set("Ordinary Drink", "assets/cocktail.svg")
+      .set("Cocktail", "assets/cocktail-logo.svg")
+      .set("Milk / Float / Shake", "assets/milkshake.svg")
+      .set("Other/Unknown", "assets/cocktail-logo.svg")
+      .set("Cocoa", "assets/cocoa.svg")
+      .set("Shot", "assets/shot.svg")
+      .set("Coffee / Tea", "assets/tea.svg")
+      .set("Homemade Liqueur", "assets/cocktail.svg")
+      .set("Punch / Party Drink", "assets/cocktail-straw.svg")
+      .set("Beer", "assets/beer.svg")
+      .set("Soft Drink / Soda", "assets/soft-drin.svg");
 
 
   
